fdab51afaed8d370006aad390e56aaa1bb726e21
bryson83 <92278513+bryson83@users.noreply.github.com>
2021-11-04 22:34:46 -0400

    Update client.lua

--- bryson-roles/client/client.lua
+++ bryson-roles/client/client.lua
@@ -3,6 +3,6 @@ AddEventHandler("playerSpawned", function()
     if not triggered then 
         triggered = true;
         Citizen.Wait((1000 * 20)); -- Wait 20 seconds
-        TriggerServerEvent('Badger_Discord_API:PlayerLoaded');
+        TriggerServerEvent('Bryson_Discord_API:PlayerLoaded');
     end
 end)
